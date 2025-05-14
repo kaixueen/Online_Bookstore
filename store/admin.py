5d90141bcc1c6cd6882b226a7efdacb30aa529a1
kaixueen <kaixuen496@gmail.com>
2025-05-14 21:48:20 +0800

    Register models in admin.py

--- store/admin.py
+++ store/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Book, Slider, CustomerProfile, Order, Payment
+from .models import Book, Slider, CustomerProfile, Order, Payment, SellerProfile, LoginActivityLog, PaymentLog
 
 admin.site.register(Book)
 admin.site.register(Slider)
 admin.site.register(CustomerProfile)
+admin.site.register(SellerProfile)
 admin.site.register(Order)
 admin.site.register(Payment)
+admin.site.register(PaymentLog)
+admin.site.register(LoginActivityLog)
+
